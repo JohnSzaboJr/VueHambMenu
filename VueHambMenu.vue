52415b8d14ad87c4a642e0782f3e42281d9290ec
John Szabo <john.szabo.jr@gmail.com>
2019-08-06 18:29:25 +0200

    first commit

---
+++ VueHambMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="vue-hamb-menu">
+    <i class="material-icons">menu</i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueHambMenu',
+}
+</script>
+
+<style>
+
+.vue-hamb-menu {
+
+}
+
+</style>
